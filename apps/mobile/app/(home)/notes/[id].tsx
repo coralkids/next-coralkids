a31b7f65efbc16083d581bdc8cc20863e5b7904d
Rubén Soler <r.solerginer@gmail.com>
2024-12-17 18:19:31 +0100

    fix header note

--- apps/mobile/app/(home)/notes/[id].tsx
+++ apps/mobile/app/(home)/notes/[id].tsx
@@ -127,6 +127,7 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: "#0D87E1",
     height: 67,
+    width: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
