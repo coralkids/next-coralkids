5aabfefe851838990718992e67c12fbcd0f1f889
Rubén Soler <r.solerginer@gmail.com>
2024-12-18 08:21:04 +0100

    remove button

--- apps/mobile/app/(auth)/_layout.tsx
+++ apps/mobile/app/(auth)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Slot } from "expo-router";
 import { SignedIn, SignedOut } from "@clerk/clerk-expo";
 import React from "react";
-import { Button } from "react-native-paper";
 
 export default function AuthRoutesLayout() {
   return (
@@ -10,13 +9,6 @@ export default function AuthRoutesLayout() {
         <Redirect href="/(home)" />
       </SignedIn>
       <SignedOut>
-        <Button
-          icon="camera"
-          mode="elevated"
-          onPress={() => console.log("Pressed")}
-        >
-          Press me
-        </Button>
         <Slot />
       </SignedOut>
     </>
