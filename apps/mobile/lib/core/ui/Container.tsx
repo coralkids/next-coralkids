580d0f73f166ed73a758e646969d433fc1767aed
Rubén Soler <r.solerginer@gmail.com>
2024-12-19 11:34:38 +0100

    style container

--- apps/mobile/lib/core/ui/Container.tsx
+++ apps/mobile/lib/core/ui/Container.tsx
@@ -1,9 +1,11 @@
+import { spacing } from "@/theme/spacing";
 import { View } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled(View)`
   max-width: 800px;
   width: 100%;
+  padding: ${spacing}px;
   align-items: start;
   margin: 0 auto;
 `;
