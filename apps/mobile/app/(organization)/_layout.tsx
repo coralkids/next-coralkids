4a8d93d76cb859b9b32ac619f045165c4456a24c
Rubén Soler <r.solerginer@gmail.com>
2024-12-29 12:05:58 +0100

    remove user provider

--- apps/mobile/app/(organization)/_layout.tsx
+++ apps/mobile/app/(organization)/_layout.tsx
@@ -1,13 +1,10 @@
 import AuthProtect from "@/lib/core/ui/AuthProtect";
-import UserProvider from "@/lib/user/providers/UserProvider";
 import { Slot } from "expo-router";
 
 export default function Layout() {
   return (
     <AuthProtect>
-      <UserProvider>
-        <Slot />
-      </UserProvider>
+      <Slot />
     </AuthProtect>
   );
 }
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,12 +1,9 @@
 import AuthProtect from "@/lib/core/ui/AuthProtect";
 import OrganizationMemberhipHome from "@/lib/organization-membership/features/OrganizationMembershipHome";
-import UserProvider from "@/lib/user/providers/UserProvider";
 
 const Home = () => (
   <AuthProtect>
-    <UserProvider>
-      <OrganizationMemberhipHome />
-    </UserProvider>
+    <OrganizationMemberhipHome />
   </AuthProtect>
 );
 
